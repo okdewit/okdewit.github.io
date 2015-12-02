099d133b72777ed37240ff598857804a150eec84
Orian de Wit <okdewit@gmail.com>
2015-12-02 15:30:09 +0100

    removed old tags example

--- tags/js/index.js
+++
@@ -1,6 +0,0 @@
-(function( $, window, document, undefined ) {
-    $( document ).ready(function() {
-        var t = $( "#tag" ).tagging({'edit-on-delete':false});
-        t[0].addClass( "form-control" );
-    });
-})( window.jQuery, window, document );
